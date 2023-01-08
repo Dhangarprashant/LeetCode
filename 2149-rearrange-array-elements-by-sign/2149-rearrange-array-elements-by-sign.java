65d5ca61d1f585bccf17ed1e7ce29d6b75609389
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-01-08 22:30:27 +0530

    Time: 7 ms (59.96%), Space: 80.1 MB (70.30%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,20 +1,44 @@
+// class Solution 
+// {
+//     public int[] rearrangeArray(int[] nums) 
+//     {
+//         int n=nums.length;
+//         int[] ans=new int[n];
+//         int pi=0;
+//         int ni=1;
+//         for(int i:nums){
+//             if(i>=0){
+//                 ans[pi]=i;
+//                 pi+=2;
+//             }else{
+//                 ans[ni]=i;
+//                 ni+=2;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution 
 {
     public int[] rearrangeArray(int[] nums) 
     {
-        int n=nums.length;
-        int[] ans=new int[n];
-        int pi=0;
-        int ni=1;
-        for(int i:nums){
-            if(i>=0){
-                ans[pi]=i;
-                pi+=2;
-            }else{
-                ans[ni]=i;
-                ni+=2;
+        int length=nums.length;
+        int a[]=new int[length];
+        int index=0;
+        for(int i=0;i<length;i++){
+            if(nums[i]>0){
+                a[index]=nums[i];
+                index=index+2;
             }
         }
-        return ans;
+         index=1;
+        for(int i=0;i<length;i++){
+            if(nums[i]<0){
+                a[index]=nums[i];
+                    index=index+2;
+            }
+        }
+        return a;
     }
 }
