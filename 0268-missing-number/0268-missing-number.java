f6ccf8e3f18094d5135a038897e699d66ab42b45
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-02-05 12:40:21 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (92.82%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,37 +1,30 @@
-// class Solution 
-// {
-//     public int missingNumber(int[] nums) 
-//     {
-//         int length=nums.length;
-//         int total=0;
-//         for(int i=0;i<length;i++){
-//             total+=i;
-//         }
-//         int arraytotal=0;
-//         if(length==2){
-//             return 2;
-//         }else if(length==1){
-//             return 0;
-//         }
-//         else{
-//             for(int j=0;j<length-1;j++){
-//             arraytotal+=j;
-//         }
-//         }
-//         return total-arraytotal;
-//     }
-// }
-
 class Solution 
 {
     public int missingNumber(int[] nums) 
     {
         int n=nums.length;
-        int total=(n*(n+1))/2;
-        int numtotal=0;
-        for(int i=0;i<n;i++){
-            numtotal+=nums[i];
+        int total=0;
+        for(int i=0;i<n+1;i++){
+            total+=i;
         }
-        return total-numtotal;
+        int arraytotal=0;
+        for(int j=0;j<n;j++){
+            arraytotal+=nums[j];
+        }
+        return total-arraytotal;
     }
 }
+
+// class Solution 
+// {
+//     public int missingNumber(int[] nums) 
+//     {
+//         int n=nums.length;
+//         int total=(n*(n+1))/2;
+//         int numtotal=0;
+//         for(int i=0;i<n;i++){
+//             numtotal+=nums[i];
+//         }
+//         return total-numtotal;
+//     }
+// }
