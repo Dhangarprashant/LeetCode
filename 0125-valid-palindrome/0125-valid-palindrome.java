3b60c7c9350804db0119a1f8d91eaf01a02dad20
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-02-21 09:55:22 +0530

    Time: 3 ms (96.77%), Space: 42 MB (93.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,10 +2,10 @@
 //     public boolean isPalindrome(String s) {
 //         char[] ch = s.toCharArray();
 //         int length=ch.length;
-//         int start=0;
-//         int end=length-1;
-//         for(int i=0;i<length;i++){
-//             for(int j=length;j>0;j--){
+//         // int start=0;
+//         // int end=length-1;
+//         for(int i=0;i<length-1;i++){
+//             for(int j=length-1;j>0;j--){
 //                 if(i<=j){
 //                     if(ch[i]==ch[j]){
 //                     return true;
@@ -42,16 +42,40 @@
 
 
 
-class Solution {
-    public boolean isPalindrome(String s) {
-        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-        String rev = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            rev+= s.charAt(i);
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+//         String rev = "";
+//         for (int i = s.length() - 1; i >= 0; i--) {
+//             rev+= s.charAt(i);
+//         }
+//         if (s.equals(rev)){
+//             return true;
+//         }
+//         return false;
+//     }
+// }
+
+
+class Solution 
+{
+    public boolean isPalindrome(String s) 
+    {
+        int left=0;
+        int right=s.length()-1;
+        while(left<=right){
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }else if(!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }else{
+                if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+                    return false;
+                }
+                left++;
+                right--;
+            }
         }
-        if (s.equals(rev)){
-            return true;
-        }
-        return false;
+        return true;
     }
 }
