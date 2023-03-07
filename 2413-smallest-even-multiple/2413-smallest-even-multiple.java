afb597094085bc03720e01c161dd6e71079c455b
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-03-07 22:44:01 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (37.93%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.java
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -1,9 +1,15 @@
 class Solution {
-    public int smallestEvenMultiple(int n) {
-        if(n%2==0){
-            return n;
+    public boolean isThree(int n) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        if(count==3){
+            return true;
         }else{
-            return n*2;
+            return false;
         }
     }
 }
