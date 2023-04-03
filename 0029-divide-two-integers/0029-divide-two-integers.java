e82b0b0d13fa9de56d41b7bb1e6fe21a6fbecbab
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-04-03 09:46:42 +0530

    Time: 1 ms (100.00%), Space: 40.3 MB (14.33%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,22 +1,19 @@
-// class Solution 
-// {
-//     public int divide(int dividend, int divisor) 
-//     {
-//         if(dividend==-2147483648 && divisor==-1){
-//             return (2147483648);
-//         }
-//         int prashant=dividend/divisor;
-//         return prashant;
-//     }
-// }
-
-
 class Solution {
     public int divide(int dividend, int divisor) {
-        if (dividend == -2147483648 && divisor == -1) {
-        return (2147483647);
-    }
-        int a = dividend/divisor;
-        return a;
+        if(dividend== 1<<31 && divisor ==-1)
+            return Integer.MAX_VALUE;
+        boolean sign=(dividend>=0) == (divisor>=0) ? true:false;
+        dividend=Math.abs(dividend);
+        divisor=Math.abs(divisor);
+        int result=0;
+        while(dividend-divisor>=0){
+            int count=0;
+            while(dividend-(divisor<<1<<count)>=0){
+                count++;
+            }
+            result +=1<<count;
+            dividend -=divisor<<count;
+        }
+        return sign?result: -result;
     }
 }
