39f9712fc865bbcd20f964a90327791b5140ff63
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-04-11 09:21:45 +0530

    Time: 0 ms (100.00%), Space: 50 MB (23.12%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -1,11 +1,30 @@
+// class Solution {
+//     public int hIndex(int[] citations) {
+//         int N=citations.length;
+//         for(int i=0;i<N;i++){
+//             if(N-i<=citations[i]){
+//                 return N-i;
+//             }
+//         }
+//         return 0;
+//     }
+// }
+
 class Solution {
     public int hIndex(int[] citations) {
-        int N=citations.length;
-        for(int i=0;i<N;i++){
-            if(N-i<=citations[i]){
-                return N-i;
+        int ans=0;
+        int start=0;
+        int n=citations.length;
+        int end=n;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(n-mid<=citations[mid]){
+                ans=Math.max(n-mid,ans);
+                end=mid;
+            }else{
+                start=mid+1;
             }
         }
-        return 0;
+        return ans;
     }
 }
