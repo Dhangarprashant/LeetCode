ea0a788cfa94ba3c0a803e2ed3dd083fae708c3c
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-04-11 09:43:39 +0530

    Time: 2 ms (82.90%), Space: 40.2 MB (34.16%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,32 +1,20 @@
-// class Solution {
-//     public int arrangeCoins(int n) {
-//         int count=0;
-//         while(n>0){
-//             for(int i=1;i<n;i++){
-//                 if(n>i){
-//                     n--;
-//                     count++;
-//                 }else{
-//                     break;
-//                 }
-//             }
-//         }
-//         return count;
-//     }
-// }
-
 class Solution {
     public int arrangeCoins(int n) {
-        long start=1,end=n,mid,ans=0;
-        while(start<=end){
-            mid = start +(end-start)/2;
-            if((mid*(mid+1))/2<=n){
-                ans=mid;
-                start=mid+1;
-            }else{
-                end=mid-1;
+        long left=0;
+        long right=n;
+        while(left<=right){
+            long mid=left+(right-left)/2;
+            long k=mid*(mid+1)/2;
+            if(k==n){
+                return (int)mid;
+            }
+            else if(k>n){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
             }
         }
-        return (int)ans;
+        return (int)right;
     }
 }
