e8183246607ccf8b613779e6a69b1ef030829955
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-04-12 09:27:22 +0530

    Time: 12 ms (34.25%), Space: 47.8 MB (24.62%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,29 +1,30 @@
-// class Solution {
-//     public int majorityElement(int[] nums) {
-//         HashMap<Integer,Integer> map=new HashMap<>();
-//         for(int i=0;i<nums.length;i++){
-//             map.put(nums[i],getOrDefualt(nums[i],0)+1);
-//         }
-//         for (int i : map.values()) {
-//             if(i>nums.length/2){
-//                 return i;
-//             }
-//          }
-//         return 0;
-//     }
-// }
-
 class Solution {
     public int majorityElement(int[] nums) {
-        int majority=0;
-        int count=1;
-        for(int i=1;i<nums.length;i++){
-            count+=nums[i]==nums[majority]?1:-1;
-            if(count==0){
-                majority=++i;
-                count++;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(int i:map.keySet()){
+            if(map.get(i)>nums.length/2){
+                return i;
             }
         }
-        return nums[majority];
+        return -1;
     }
 }
+
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int majority=0;
+//         int count=1;
+//         for(int i=1;i<nums.length;i++){
+//             count+=nums[i]==nums[majority]?1:-1;
+//             if(count==0){
+//                 majority=++i;
+//                 count++;
+//             }
+//         }
+//         return nums[majority];
+//     }
+// }
