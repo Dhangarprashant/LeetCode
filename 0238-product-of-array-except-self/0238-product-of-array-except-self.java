38c343d8c43375d01cdbfa6c39f5c3831d8e92f4
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-04-13 08:33:02 +0530

    Time: 1 ms (100.00%), Space: 50.8 MB (48.17%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,47 +1,34 @@
 // class Solution {
 //     public int[] productExceptSelf(int[] nums) {
-//         int length=nums.length;
-//         int[] array=new int[length];
-//         int totalM=1;
-//         int finalM=1;
-//        for(int i=0;i<length;i++){
-//            totalM=nums[i]*totalM;
-//            finalM=finalM*totalM;
-//            array[i]=finalM;
-//        }
-// //         for(int i=0;i<length;++){
-            
-// //         }
-//         return array;
+//         int pro=1;
+//         for(int i=0;i<nums.length;i++){
+//             pro=pro*nums[i];
+//         }
+//         int result[]=new int[nums.length];
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]!=0){
+//                 result[i]=pro/nums[i];
+//             }else{
+//                 result[i]=pro;
+//             }
+//         }
+//         return result;
 //     }
 // }
 
-
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int pre[]=new int[nums.length];
-        int post[]=new int[nums.length];
-
-        //keep track of the total product on the left side of each num
-        int temp=1, prod=1;
-        for(int i=0;i<pre.length;i++){
-            prod*=temp;
-            pre[i]=prod;
-            temp=nums[i];
-        }
-
-        //keep track of the total product on the right side of each num
-        temp=1; prod=1;
-        for(int i=post.length-1;i>=0;i--){
-            prod*=temp;
-            post[i]=prod;
-            temp=nums[i];
-        }
-
+        int temp=1;
+        int[] result=new int[nums.length];
         for(int i=0;i<nums.length;i++){
-            nums[i]=pre[i]*post[i];
+            result[i]=temp;
+            temp=temp*nums[i];
         }
-
-        return nums;
+        temp=1;
+        for(int i=nums.length-1;i>=0;i--){
+            result[i]=result[i]*temp;
+            temp=temp*nums[i];
+        }
+        return result;
     }
 }
