03c13a39985dff0b3544fcd3041fd38676569316
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-04-15 21:37:01 +0530

    Time: 24 ms (47.16%), Space: 43.1 MB (19.71%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -1,37 +1,21 @@
-// class Solution {
-//     public int mostFrequentEven(int[] nums) {
-//         Arrays.sort(nums);
-//         HashMap<Integer,Integer> map=new HashMap<>();
-//         for(int i=0;i<nums.length;i++){
-//             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-//         }
-//         int max=-1;
-//         int ans=-1;
-//         for(int i:map.keySet()){
-//             if(i%2==0){
-//                  ans=Math.max(max,map.get(i));
-//                 return ans;
-//             }
-//         }
-//         return ans;
-//     }
-// }
-
 class Solution {
     public int mostFrequentEven(int[] nums) {
-        int ans[]=new int[100001];
-        for(int x:nums){
-            ans[x]++;
-        }
-        int maxcount=0;
-        int res=-1;
-        for(int i=0;i<ans.length;i++){
-            if(i%2==0 && ans[i]>maxcount){
-                maxcount=ans[i];
-                res=i;
+        int res=Integer.MAX_VALUE;
+        int count=0;
+        HashMap<Integer,Integer> map= new HashMap<>();
+        for(int i:nums){
+            if(i%2==0){
+                map.put(i,map.getOrDefault(i,0)+1);
+                if(count<map.get(i)){
+                    count=map.get(i);
+                    res=i;
+                }else if(count==map.get(i)){
+                    res=Math.min(res,i);
+                }
             }
         }
-        return res;
+         if(map.isEmpty()) return -1; 
+        return res; 
     }
 }
 
