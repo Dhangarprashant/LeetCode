d0807177745e432542eeaa0e54824d4dd51f9ad2
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-09-16 07:40:03 +0530

    Added solution - LeetHub

--- Count number of hops - GFG/count-number-of-hops.java
+++ Count number of hops - GFG/count-number-of-hops.java
@@ -36,56 +36,58 @@ class GFG implements Runnable {
 //User function Template for Java
 
 
-class Solution
-{
-    //Function to count the number of ways in which frog can reach the top.
-    static long mod=1000000007;
-    static long countWays(int n)
-    {
-        // add your code here
-        long a=1;
-        long b=2;
-        long c=4;
-        if(n==1)
-        return a;
-         if(n==2)
-        return b;
-         if(n==3)
-        return c;
-        
-        long d=0;
-        for(int i=4;i<=n;i++){
-            d=(((a%mod+b%mod)%mod)%mod+c%mod)%mod;
-            a=b%mod;
-            b=c%mod;
-            c=d%mod;
-        }
-        return d%mod;
-    }
-    
-}
-
 // class Solution
 // {
 //     //Function to count the number of ways in which frog can reach the top.
-//   static long mod = 1000000007;
+//     static long mod=1000000007;
 //     static long countWays(int n)
 //     {
-//         // add your code here
-//          long a=1; long b=2; long c=4;
-//         if(n==1) return a;
-//         if(n==2) return b;
-//         if(n==3) return c;
+//         long a=1;
+//         long b=2;
+//         long c=4;
+//         if(n==1){
+//             return a;
+//         }
+//         if(n==2){
+//             return b;
+//         }
+//         if(n==4){
+//             return c;
+//         }
         
-//         long d=0;
-//       for( int i=4; i<=n; i++)
-//       {
-//           d= ( ((a%mod+b%mod)%mod)%mod + c%mod) %mod;
-//           a=b%mod;
-//           b=c%mod;
-//           c=d%mod;
-//       }
-       
-//         return d%mod;
+//         long dp[]=new long[n];
+//         dp[0]=1;
+//         dp[1]=2;
+//         dp[2]=4;
+//         for(int i=3;i<n;i++){
+//             dp[i]=(((dp[i-1]%mod+dp[i-2]%mod)%mod)%mod+dp[i-3]%mod)%mod;
+//         }
+//         return dp[n-1];
 //     }
 // }
+
+class Solution
+{
+    //Function to count the number of ways in which frog can reach the top.
+  static long mod = 1000000007;
+    static long countWays(int n)
+    {
+        // add your code here
+        long dp[] = new long[n];
+       
+        if(n==1) return 1;
+        if(n==2) return 2;
+        if(n==3) return 4;
+        
+        dp[0]=1; dp[1] =2; dp[2]=4;
+        
+        long d=0;
+      for( int i=3; i<n; i++)
+      {
+          dp[i]= ( ((dp[i-1]%mod+dp[i-2]%mod)%mod)%mod + dp[i-3]%mod) %mod;
+      }
+       
+        return dp[n-1];
+    }
+}
+
