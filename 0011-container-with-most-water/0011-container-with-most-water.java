5166949e688ebeccbfa64dbc8434ff055838ec46
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-11-01 12:06:14 +0530

    Time: 5 ms (47.00%), Space: 55.4 MB (70.69%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,52 +1,20 @@
-// class Solution {
-//     public int maxArea(int[] height) {
-//         int total=0;
-//         for(int i=0;i<height.length;i++){
-//             for(int j=i+1;j<height.length;j++){
-//                 int col=Math.min(height[i],height[j]);
-//                 int row=j-i;
-//                 int volume=col*row;
-//                 if(volume>total){
-//                     total=volume;
-//                 }
-//             }
-//         }
-//         return total;
-//     }
-// }
-
-// class Solution {
-//     public int maxArea(int[] height) {
-//         int maxArea = 0;
-        
-//         for (int i = 0; i < height.length; i++) {
-//             for (int j = i + 1; j < height.length; j++) {
-//                 int minHeight = Math.min(height[i], height[j]);
-//                 int width = j - i;
-//                 int currentArea = minHeight * width;
-//                 maxArea = Math.max(maxArea, currentArea);
-//             }
-//         }
-        
-//         return maxArea;
-//     }
-// }
-
-
 class Solution {
     public int maxArea(int[] height) {
-        int water=0;
-        int i=0;
-        int j=height.length-1;
-        while(i<=j){
-            int volume=(j-i)*Math.min(height[i],height[j]);
-            water=Math.max(water,volume);
-            if(height[i]<height[j]){
-                i++;
+        int left=0;
+        int right=height.length-1;
+        int area;
+        int maxarea=0;
+        while(left<=right){
+            area=Math.min(height[left],height[right])*(right-left);
+            if(area>maxarea){
+                maxarea=area;
+            }
+            if(Math.min(height[left],height[right])==height[left]){
+                left++;
             }else{
-                j--;
+                right--;
             }
         }
-        return water;
+        return maxarea;
     }
 }
