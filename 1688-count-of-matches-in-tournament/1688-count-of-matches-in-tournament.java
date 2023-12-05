6f97f2fe3b2c9edcedec6bf50377f4d0b4a66dbb
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-12-05 11:18:50 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (17.85%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -1,16 +1,30 @@
+// class Solution {
+//     public int numberOfMatches(int n) {
+//         int count=0;
+//         int temp=n;
+//         while(temp!=0){
+//             if(temp%2==0){
+//                 count=temp/2;
+//                 temp=temp/2;
+//             }else{
+//                 count=temp/2;
+//                 temp=temp/2+1;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
     public int numberOfMatches(int n) {
-        // int count=0;
-        // int temp=n;
-        // while(temp!=0){
-        //     if(temp%2==0){
-        //         count=temp/2;
-        //         temp=temp/2;
-        //     }else{
-        //         count=temp/2;
-        //         temp=temp/2+1;
-        //     }
-        // }
-        return n-1;
+        int count = 0;
+        while (n > 1) {
+            // Update count with the number of matches in the current round
+            count += n / 2;
+            
+            // Calculate the number of teams for the next round
+            n = (n + 1) / 2;
+        }
+        return count;
     }
 }
