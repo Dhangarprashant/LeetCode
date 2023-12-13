1c67720e4b41e5d7e5736af3a61fa09dd3d7e7de
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2023-12-13 12:20:22 +0530

    Time: 680 ms (84.85%), Space: 0B (100.00%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,11 +1,19 @@
 # Write your MySQL query statement below
 
-with cte as
-(select num
-from Mynumbers
-group by num
-having count(num)=1)
+# with cte as
+# (select num
+# from Mynumbers
+# group by num
+# having count(num)=1)
 
-select case when count(*)>0 then max(num)
-else null end as num
-from cte;
+# select case when count(*)>0 then max(num)
+# else null end as num
+# from cte;
+
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+) AS unique_numbers;
