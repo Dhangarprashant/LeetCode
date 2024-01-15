e721a229811cd99525d9c1d5c129c69bdb11661c
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-01-15 19:30:25 +0530

    Time: 465 ms (65.20%), Space: 0B (100.00%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -3,4 +3,8 @@
 
 SELECT name,population,area
 FROM World
-where area>=3000000 OR population>=25000000;
+where area>=3000000
+UNION
+SELECT name,population,area
+FROM World
+where population>=25000000
