592c7261a85e9b868968654ec570f302f14888e1
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-01-15 19:57:28 +0530

    Time: 1221 ms (35.34%), Space: 0B (100.00%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,6 +1,13 @@
 # Write your MySQL query statement below
 
+# this is not correct becuase here length is counting bytes which is not correct ,we want count of characters
+# SELECT tweet_id
+# FROM Tweets
+# WHERE LENGTH(content)>15;
+
+
+# this is correct beacause here we are using char_length which count only characters
 
 SELECT tweet_id
 FROM Tweets
-WHERE LENGTH(content)>15;
+WHERE CHAR_LENGTH(content)>15;
