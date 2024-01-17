ae59eab557053f4738d2eac64d096a139278ab48
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-01-17 10:21:48 +0530

    Time: 2 ms (98.10%), Space: 41.7 MB (21.99%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -4,14 +4,32 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
-        HashSet<Integer> set=new HashSet<>();
-        for(int i:map.keySet()){
-            if(set.contains(map.get(i))){
+        Set<Integer> set=new HashSet<>();
+        for(int i:map.values()){
+            if(set.contains(i)){
                 return false;
             }else{
-                set.add(map.get(i));
+                set.add(i);
             }
         }
         return true;
     }
 }
+
+// class Solution {
+//     public boolean uniqueOccurrences(int[] arr) {
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         for(int i=0;i<arr.length;i++){
+//             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+//         }
+//         HashSet<Integer> set=new HashSet<>();
+//         for(int i:map.keySet()){
+//             if(set.contains(map.get(i))){
+//                 return false;
+//             }else{
+//                 set.add(map.get(i));
+//             }
+//         }
+//         return true;
+//     }
+// }
