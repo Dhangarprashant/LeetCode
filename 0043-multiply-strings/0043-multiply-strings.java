777dbc0867fcadcb2c2ae791156e953514b20675
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-01-30 19:58:43 +0530

    Time: 9 ms (41.62%), Space: 42.7 MB (38.06%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,39 +1,9 @@
+import java.math.BigInteger;
 class Solution {
     public String multiply(String num1, String num2) {
-        if(num1.equals("0") || num2.equals("0")){
-            return "0";
-        }
-        int l1=num1.length();
-        int l2=num2.length();
-        int arr[]=new int[l1+l2];
-        int i=l2-1;
-        int pf=0;
-        while(i>=0){
-            int ival=num2.charAt(i)-'0';
-            i--;
-            int j=l1-1;
-            int k=arr.length-1-pf;
-            int carry=0;
-            while(j>=0 || carry!=0){
-                int jval=j>=0?num1.charAt(j)-'0':0;
-                j--;
-                int prod=ival*jval+carry+arr[k];
-                arr[k]=prod%10;
-                carry=prod/10;
-                k--;
-            }
-            pf++;
-        }
-        String str="";
-        boolean flag=false;
-        for(int val:arr){
-            if(val==0 && flag==false){
-                continue;
-            }else{
-                flag=true;
-                str=str+val;
-            }
-        }
-        return str;
+        BigInteger num11=new BigInteger(num1);
+        BigInteger num22=new BigInteger(num2);
+        BigInteger ans=num11.multiply(num22);
+        return ans.toString();
     }
 }
