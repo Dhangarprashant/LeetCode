dff1dad62c76e8dce2b64a5fcec2e5fe5e8e4a17
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-02-04 21:55:02 +0530

    Time: 1 ms (100.00%), Space: 44.3 MB (73.19%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,61 +1,33 @@
+
 class Solution {
-    public String minWindow(String s1, String s2) {
-        String ans = "";
-        HashMap<Character, Integer> map2 = new HashMap<>();
-        for (int i = 0; i < s2.length(); i++) {
-            char c = s2.charAt(i);
-            map2.put(c, map2.getOrDefault(c, 0) + 1);
+    public String minWindow(String s, String t) {
+        if (s == null || t == null || s.length() ==0 || t.length() == 0 ||
+                s.length() < t.length()) {
+            return new String();
         }
-        
-        int mct = 0;
-        int dmct = s2.length(); 
-        
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        int i = -1;
-        int j = -1;
-        
-        while (true) {
-            boolean f1 = false;
-            boolean f2 = false;
-            
-            if (i < s1.length() - 1 && mct < dmct) {
-                i++;
-                char c = s1.charAt(i);
-                map1.put(c, map1.getOrDefault(c, 0) + 1);
-                if (map1.get(c) <= map2.getOrDefault(c, 0)) {
-                    mct++;
-                }
-                f1 = true;
+        int[] map = new int[128];
+        int count = t.length();
+        int start = 0, end = 0, minLen = Integer.MAX_VALUE,startIndex =0;
+        for (char c :t.toCharArray()) {
+            map[c]++;
+        }
+        char[] chS = s.toCharArray();
+        while (end < chS.length) {
+            if (map[chS[end++]]-- >0) {
+                count--;
             }
-            
-            while (j < i && mct == dmct) {
-                String pans = s1.substring(j + 1, i + 1);
-                if (ans.length() == 0 || pans.length() < ans.length()) {
-                    ans = pans;
+            while (count == 0) {
+                if (end - start < minLen) {
+                    startIndex = start;
+                    minLen = end - start;
                 }
-                
-                j++;
-                char ch = s1.charAt(j);
-                
-                // Update map1 for the character going out of the window
-                if (map1.get(ch) == 1) {
-                    map1.remove(ch);
-                } else {
-                    map1.put(ch, map1.get(ch) - 1);
+                if (map[chS[start++]]++ == 0) {
+                    count++;
                 }
-                
-                if (map1.getOrDefault(ch, 0) < map2.getOrDefault(ch, 0)) {
-                    mct--;
-                }
-                f2 = true;
-            }
-            
-            if (!f1 && !f2) {
-                break;
             }
         }
-        
-        return ans;
+
+        return minLen == Integer.MAX_VALUE? new String():
+                new String(chS,startIndex,minLen);
     }
 }
-
