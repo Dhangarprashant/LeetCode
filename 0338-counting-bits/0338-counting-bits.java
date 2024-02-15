972acc48397d25ac53b456382b398f7c4ce0c075
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-02-15 16:32:28 +0530

    Time: 3 ms (42.10%), Space: 46.4 MB (84.81%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,22 +1,45 @@
+// class Solution {
+//     public int[] countBits(int n) {
+//         int ans[]=new int[n+1];
+//         for(int i=0;i<=n;i++){
+//             int temp=i;
+//             int number=temp;
+//             String str="";
+//             while(number!=0){
+//                 str=str+number%2;
+//                 number=number/2;
+//             }
+//             int count=0;
+//             for(int j=0;j<str.length();j++){
+//                 if(str.charAt(j)=='1'){
+//                     count++;
+//                 }
+//             }
+//             ans[temp]=count;
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int[] countBits(int n) {
-        int ans[]=new int[n+1];
-        for(int i=0;i<=n;i++){
-            int temp=i;
-            int number=temp;
-            String str="";
-            while(number!=0){
-                str=str+number%2;
-                number=number/2;
-            }
-            int count=0;
-            for(int j=0;j<str.length();j++){
-                if(str.charAt(j)=='1'){
-                    count++;
-                }
-            }
-            ans[temp]=count;
+        int[] result = new int[n + 1];
+        
+        for (int i = 0; i <= n; i++) {
+            result[i] = countSetBits(i);
         }
-        return ans;
+        
+        return result;
+    }
+    
+    private int countSetBits(int num) {
+        int count = 0;
+        
+        while (num > 0) {
+            count += num & 1;
+            num = num >> 1;
+        }
+        
+        return count;
     }
 }
