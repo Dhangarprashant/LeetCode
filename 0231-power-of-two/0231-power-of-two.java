e335a80b1f7657cf6054e6a68acb587c3b915a3d
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-02-19 08:23:39 +0530

    Time: 8 ms (11.15%), Space: 42.2 MB (8.08%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,9 +1,37 @@
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         String str="";
+//         while(n!=0){
+//             str+=n%2;
+//             n=n/2;
+//         }
+//         int count=0;
+//         for(int i=0;i<str.length();i++){
+//             if(str.charAt(i)=='1'){
+//                 count++;
+//             }
+//         }
+//         return (count==1)?true:false;
+//     }
+// }
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if (n <= 0) {
             return false;
         }
-
-        return (Math.log10(n) / Math.log10(2)) % 1 == 0;
+        String str = "";
+        while (n != 0) {
+            str += n % 2;
+            n = n / 2;
+        }
+        
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '1') {
+                count++;
+            }
+        }
+        
+        return count == 1;
     }
 }
