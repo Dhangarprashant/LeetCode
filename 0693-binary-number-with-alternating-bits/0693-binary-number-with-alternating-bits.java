2d38cc9b452e1e802fa8805c740438afd15c3ac6
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-02-19 15:46:33 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (35.08%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean hasAlternatingBits(int n) {
-        String ans="";
-        while(n!=0){
-            ans=ans+n%2;
-            n=n/2;
-        }
-        for(int i=1;i<ans.length();i++){
-            if(ans.charAt(i-1)==ans.charAt(i)){
+     
+        String ans = Integer.toBinaryString(n);
+        for(int i=1;i<ans.length();i++)
+        {
+            if(ans.charAt(i-1)==ans.charAt(i))
+            {
                 return false;
             }
         }
