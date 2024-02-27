0da3ffbf8f02a2132419b3676fea381de9982bc4
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-02-27 19:11:19 +0530

    Time: 7 ms (73.86%), Space: 40.3 MB (85.87%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
@@ -2,25 +2,23 @@ class Solution {
     public int countPrimeSetBits(int left, int right) {
         int result = 0;
         for (int i = left; i <= right; i++) {
-            String binaryRepresentation = Integer.toBinaryString(i);
-            int count = 0;
-            for (int j = 0; j < binaryRepresentation.length(); j++) {
-                if (binaryRepresentation.charAt(j) == '1') {
-                    count++;
-                }
-            }
-            if (isPrime(count)) {
+            if (isPrimeSetBits(i)) {
                 result++;
             }
         }
         return result;
     }
 
-    public static boolean isPrime(int num) {
+    private boolean isPrimeSetBits(int num) {
+        int count = Integer.bitCount(num);
+        return isPrime(count);
+    }
+
+    private boolean isPrime(int num) {
         if (num <= 1) {
             return false;
         }
-        for (int i = 2; i*i <= num; i++) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return false;
             }
@@ -28,3 +26,4 @@ class Solution {
         return true;
     }
 }
+
