5f2de624cdd5bd16d136194363cbca1d9dcc7e9f
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-03-03 12:00:09 +0530

    Time: 6 ms (64.34%), Space: 42.8 MB (85.96%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.java
@@ -0,0 +1,48 @@
+// class Solution {
+//     public String largestNumber(int[] nums) {
+//         String[] arr = new String[nums.length];
+//         for (int i = 0; i < nums.length; i++) {
+//             arr[i] = String.valueOf(nums[i]);
+//         }
+//         Arrays.sort(arr,(a,b)->{
+//             long n1=Long.parseLong(a+b);
+//             long n2=Long.parseLong(b+a);
+//             if(n1>n2){
+//                 return 1;
+//             }else if(n1<n2){
+//                 return -1;
+//             }else{
+//                 return 0;
+//             }
+//         });
+//         StringBuilder sb=new StringBuilder("");
+//         for(int i=arr.length-1;i>=0;i--){
+//             sb.append(arr[i]);
+//         }
+//         if(sb.charAt(0)=='0'){
+//             return "0";
+//         }
+//         return sb.toString();
+//     }
+// }
+
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] arr = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+        StringBuilder sb = new StringBuilder("");
+        for (String s : arr) {
+            sb.append(s);
+        }
+
+        if (sb.charAt(0) == '0') {
+            return "0";
+        }
+        return sb.toString();
+    }
+}
