29c9121a00a1d0a5d8fd1683b8c40f6dff3841da
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-03-03 12:07:59 +0530

    Time: 7 ms (32.01%), Space: 43 MB (78.35%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,36 +1,9 @@
-// class Solution {
-//     public String largestNumber(int[] nums) {
-//         String[] arr = new String[nums.length];
-//         for (int i = 0; i < nums.length; i++) {
-//             arr[i] = String.valueOf(nums[i]);
-//         }
-//         Arrays.sort(arr,(a,b)->{
-//             long n1=Long.parseLong(a+b);
-//             long n2=Long.parseLong(b+a);
-//             if(n1>n2){
-//                 return 1;
-//             }else if(n1<n2){
-//                 return -1;
-//             }else{
-//                 return 0;
-//             }
-//         });
-//         StringBuilder sb=new StringBuilder("");
-//         for(int i=arr.length-1;i>=0;i--){
-//             sb.append(arr[i]);
-//         }
-//         if(sb.charAt(0)=='0'){
-//             return "0";
-//         }
-//         return sb.toString();
-//     }
-// }
 
 class Solution {
     public String largestNumber(int[] nums) {
         String[] arr = new String[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            arr[i] = String.valueOf(nums[i]);
+            arr[i] = nums[i]+"";
         }
 
         Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
