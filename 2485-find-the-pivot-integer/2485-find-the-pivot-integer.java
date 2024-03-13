827b8df86577b49f3fe24393f40926c4d56e1dab
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-03-13 19:33:15 +0530

    Time: 1 ms (77.54%), Space: 40.2 MB (88.87%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,27 +1,45 @@
+// class Solution {
+//     public int pivotInteger(int n) {
+//         int ans=-1;
+//         for(int i=1;i<=n;i++){
+//             if(pivot(i,n)==true){
+//                 ans=i;
+//                 break;
+//             }
+//         }
+//         return ans;
+//     }
+//     public static boolean pivot(int nu,int n){
+//         int sum=0;
+//         for(int i=1;i<=nu;i++){
+//             sum=sum+i;
+//         }
+//         int sum2=0;
+//         for(int i=nu;i<=n;i++){
+//             sum2=sum2+i;
+//         }
+//         if(sum==sum2){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     }
+// }
+
 class Solution {
     public int pivotInteger(int n) {
-        int ans=-1;
-        for(int i=1;i<=n;i++){
-            if(pivot(i,n)==true){
-                ans=i;
-                break;
+        for (int i = 1; i <= n; i++) {
+            if (pivot(i, n)) {
+                return i;
             }
         }
-        return ans;
+        return -1;
     }
-    public static boolean pivot(int nu,int n){
-        int sum=0;
-        for(int i=1;i<=nu;i++){
-            sum=sum+i;
-        }
-        int sum2=0;
-        for(int i=nu;i<=n;i++){
-            sum2=sum2+i;
-        }
-        if(sum==sum2){
-            return true;
-        }else{
-            return false;
-        }
+
+    public static boolean pivot(int nu, int n) {
+        int sum1 = nu * (nu + 1) / 2;  
+        int sum2 = n * (n + 1) / 2 - sum1+nu;  
+
+        return sum1 == sum2;
     }
 }
