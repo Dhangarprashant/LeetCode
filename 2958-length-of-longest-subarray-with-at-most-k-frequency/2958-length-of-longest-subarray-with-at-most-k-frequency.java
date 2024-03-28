9b14c95223b541c5cf69b49bb76ab5ac660be813
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-03-28 09:45:01 +0530

    Time: 68 ms (59.47%), Space: 60.5 MB (57.97%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,21 +1,3 @@
-// class Solution {
-//     public int maxSubarrayLength(int[] nums, int k) {
-//         HashMap<Integer,Integer> map=new HashMap<>();
-//         int ans=0;
-//         int i=0;
-//         int j=0;
-//         while(j<nums.length){
-//             map[nums[j]]++;
-//             while(i<=j && map[nums[j]]>k){
-//                 map[nums[i]]--;
-//                 i++;
-//             }
-//             ans=Math.max(ans,j-i+1);
-//             j++;
-//         }
-//         return ans;
-//     }
-// }
 
 
 class Solution {
@@ -25,11 +7,7 @@ class Solution {
         int i=0;
         int j=0;
         while(j<nums.length){
-            if(map.containsKey(nums[j])) {
-                map.put(nums[j], map.get(nums[j]) + 1);
-            } else {
-                map.put(nums[j], 1);
-            }
+            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
             while(i<=j && map.get(nums[j]) > k){
                 map.put(nums[i], map.get(nums[i]) - 1);
                 i++;
