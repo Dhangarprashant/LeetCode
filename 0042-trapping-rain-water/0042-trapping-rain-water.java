84cdd42a148702218dcdd4614bf885614a214861
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-04-12 19:19:00 +0530

    Time: 1 ms (71.17%), Space: 45.3 MB (95.21%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,17 +1,58 @@
+// class Solution {
+//     public int trap(int[] height) {
+//         int n=height.length;
+//         int totalWater=0;
+        
+//         int prefix[]=new int[n];
+//         int suffix[]=new int[n];
+        
+//         prefix[0]=height[0];
+//         int leftmax=0;
+        
+//         for(int i=1;i<n;i++){
+//             leftmax=Math.max(leftmax,height[i]);
+//             prefix[i]=leftmax;
+//         }
+        
+//         suffix[n-1]=height[n-1];
+//         int rightmax=0;
+        
+//         for(int i=n-2;i>=0;i--){
+//             rightmax=Math.max(rightmax,height[i]);
+//             suffix[i]=rightmax;
+//         }
+//         for(int i=0;i<n;i++){
+//             totalWater+=Math.min(prefix[i],suffix[i])-height[i];
+//         }
+//         return totalWater;
+//     }
+// }
+
 class Solution {
     public int trap(int[] height) {
-        int totalWater=0;
         int n=height.length;
+        int totalWater=0;
+        
+        int prefix[]=new int[n];
+        int suffix[]=new int[n];
+        
+        prefix[0]=height[0];
+        int leftmax=height[0];
+        
+        for(int i=1;i<n;i++){
+            leftmax=Math.max(leftmax,height[i]);
+            prefix[i]=leftmax;
+        }
+        
+        suffix[n-1]=height[n-1];
+        int rightmax=height[n-1];
+        
+        for(int i=n-2;i>=0;i--){
+            rightmax=Math.max(rightmax,height[i]);
+            suffix[i]=rightmax;
+        }
         for(int i=0;i<n;i++){
-            int leftMax=0;
-            int rightMax=0;
-            for(int j=i;j>=0;j--){
-                leftMax=Math.max(leftMax,height[j]);
-            }
-            for(int j=i;j<n;j++){
-                rightMax=Math.max(rightMax,height[j]);
-            }
-            totalWater+=Math.min(leftMax,rightMax)-height[i];
+            totalWater+=Math.min(prefix[i],suffix[i])-height[i];
         }
         return totalWater;
     }
