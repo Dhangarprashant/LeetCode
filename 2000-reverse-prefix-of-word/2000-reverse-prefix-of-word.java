799fdc951ac69f2e935258c9986d91e75ad13e74
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-05-01 12:38:40 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (23.84%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -6,7 +6,17 @@ class Solution {
             return word;
         }
         
-        StringBuilder sb = new StringBuilder(word.substring(0, index + 1));
-        return sb.reverse().toString() + word.substring(index + 1);
+        char[] chars = word.toCharArray();
+        int i = 0;
+        int j = index;
+        while (i < j) {
+            char temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+            i++;
+            j--;
+        }
+        
+        return new String(chars);
     }
 }
