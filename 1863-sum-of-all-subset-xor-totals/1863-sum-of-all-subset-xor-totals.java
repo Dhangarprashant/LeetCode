d1735981d744008756bcda5bf4f2d0bc5eb40636
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-05-20 10:24:56 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (96.23%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,29 +1,15 @@
 
 class Solution {
     public int subsetXORSum(int[] nums) {
-        List<List<Integer>> subsets = new ArrayList<>();
-        generateSubsets(nums, 0, new ArrayList<>(), subsets);
-        int sum = 0;
-        for (List<Integer> subset : subsets) {
-            sum += calculateXOR(subset);
-        }
-        return sum;
+        return calculateXORSum(nums, 0, 0);
     }
-    private void generateSubsets(int[] nums, int index, List<Integer> current, List<List<Integer>> subsets) {
+    private int calculateXORSum(int[] nums, int index, int currentXOR) {
         if (index == nums.length) {
-            subsets.add(new ArrayList<>(current));
-            return;
+            return currentXOR;
         }
-        generateSubsets(nums, index + 1, current, subsets);
-        current.add(nums[index]);
-        generateSubsets(nums, index + 1, current, subsets);
-        current.remove(current.size() - 1);
-    }
-    private int calculateXOR(List<Integer> subset) {
-        int xorTotal = 0;
-        for (int num : subset) {
-            xorTotal ^= num;
-        }
-        return xorTotal;
+        int withCurrent = calculateXORSum(nums, index + 1, currentXOR ^ nums[index]);
+        int withoutCurrent = calculateXORSum(nums, index + 1, currentXOR);
+        
+        return withCurrent + withoutCurrent;
     }
 }
