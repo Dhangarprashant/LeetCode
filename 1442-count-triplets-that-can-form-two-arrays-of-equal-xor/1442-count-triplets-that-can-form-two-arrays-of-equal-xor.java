ffef6266f3e48bfff135ed5535185e75c09b2f79
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-05-30 09:14:44 +0530

    Time: 34 ms (9.74%), Space: 40.5 MB (97.44%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
@@ -1,12 +1,20 @@
 class Solution {
     public int countTriplets(int[] arr) {
-        int count=0;
-        for(int i=0;i<arr.length;i++){
-            int val=arr[i];
-            for(int k=i+1;k<arr.length;k++){
-                val=val^arr[k];
-                if(val==0){
-                    count=count+(k-i);
+        int n = arr.length;
+        int count = 0;
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+                int a = 0;
+                for (int k = i; k < j; k++) {
+                    a ^= arr[k];
+                }
+                int b = 0;
+                for (int k = j; k < n; k++) {
+                    b ^= arr[k];
+                    if (a == b) {
+                        count++;
+                    }
                 }
             }
         }
