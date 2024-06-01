6a7aa5f58fbff35ed6a454f0e9a21d2c02de1216
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-06-01 12:19:18 +0530

    Time: 79 ms (34.42%), Space: 58.9 MB (58.11%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,19 +3,12 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let left=new Array(prices.length);
-    let right=new Array(prices.length);
-    left[0]=prices[0];
-    right[prices.length-1]=prices[prices.length-1];
-    for(let i=1;i<prices.length;i++){
-        left[i]=Math.min(left[i-1],prices[i]);
-    }
-    for(let i=prices.length-2;i>=0;i--){
-        right[i]=Math.max(right[i+1],prices[i]);
-    }
-    let max=0;
+    let maxProfit=0;
+    var sofar=prices[0];
     for(let i=0;i<prices.length;i++){
-        max=Math.max(max,(right[i]-left[i]));
+        sofar=Math.min(sofar,prices[i]);
+        let profit=prices[i]-sofar;
+        maxProfit=Math.max(maxProfit,profit);
     }
-    return max;
+    return maxProfit;
 };
