bf2a0dc7935e6964c2159cd506367db9f14bdcb1
Prashant dhangar <91205920+Dhangarprashant@users.noreply.github.com>
2024-07-30 22:55:41 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (81.36%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -1,19 +1,15 @@
-
 class Solution {
     public boolean checkString(String s) {
-        int n = s.length();
+        boolean foundB = false;
         
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == 'b') {
-                for (int j = i + 1; j < n; j++) {
-                    if (s.charAt(j) == 'a') {
-                        return false;
-                    }
-                }
+        for (char c : s.toCharArray()) {
+            if (c == 'b') {
+                foundB = true;
+            } else if (foundB && c == 'a') {
+                return false;
             }
         }
         
         return true;
     }
-
 }
